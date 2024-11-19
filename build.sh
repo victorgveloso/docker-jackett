60b9968d2bfefd520ba4e728740a4e4479593699
Victor Guerra Veloso <victorgvbh@gmail.com>
2024-11-19 13:15:09 -0500

    Update build cmd to reflect packaging in tarball

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 EnableWindowsTargeting=true dotnet publish --self-contained --runtime linux-musl-x64 --framework net8.0 --tl:on Jackett.sln
+cp ../*.* Jackett.Server/bin/Release/net8.0/linux-musl-x64/publish/
+pushd Jackett.Server/bin/Release/net8.0/linux-musl-x64/
+rm -rf Jackett
+mv publish Jackett
+tar --zstd -cvf Jackett.tar.zstd Jackett
+popd
+mv Jackett.Server/bin/Release/net8.0/linux-musl-x64/Jackett.tar.zstd .
